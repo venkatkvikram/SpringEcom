9fa7a18fb05fc18931c36136485aa8ef048ef0a1
venkatkvikram <venkatsai.esampalli@gmail.com>
2026-08-14 01:49:01 +0530

    Appended Image field to Product Table and

--- src/main/java/com/upskill/SpringEcom/Service/ProductService.java
+++ src/main/java/com/upskill/SpringEcom/Service/ProductService.java
@@ -5,7 +5,9 @@ import com.upskill.SpringEcom.Model.Product;
 import com.upskill.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,5 +22,13 @@ public class ProductService {
 
     public Product getProductById(int id){
         return productRepo.findById(id).get();
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
     }
 }
--- src/main/java/com/upskill/SpringEcom/controller/ProductController.java
+++ src/main/java/com/upskill/SpringEcom/controller/ProductController.java
@@ -6,11 +6,10 @@ import com.upskill.SpringEcom.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -36,6 +35,18 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        Product saveProduct = null;
+        try {
+            saveProduct = productService.addProduct(product, image);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(saveProduct, HttpStatus.CREATED);
+    }
+
 
 
 }
